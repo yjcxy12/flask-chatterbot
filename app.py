901bb4a2bd41fc81b1c726be256ac4c0ea5cffbe
Xiangyun Chi <xiangyun.chi@sky.uk>
2017-05-03 13:04:40 +0100

    add more training data

--- app.py
+++ app.py
@@ -1,25 +1,39 @@
 from flask import Flask, render_template
 from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+import json
+from pprint import pprint
+
+with open('corpus.json') as data_file:    
+   data = json.load(data_file)
 
 app = Flask(__name__)
 
-english_bot = ChatBot(
-	"English Bot",
+zhuzhu_bot = ChatBot(
+  "Zhuzhu Bot",
+  storage_adapter={
+      'import_path': 'chatterbot.storage.MongoDatabaseAdapter',
+      'database_name': 'chatterbot-database',
+      'database_uri': 'mongodb://178.62.126.50'
+  },
   logic_adapters=[
       {
           'import_path': 'chatterbot.logic.BestMatch'
       },
       {
           'import_path': 'chatterbot.logic.LowConfidenceAdapter',
-          'threshold': 0.65,
+          'threshold': 0.35,
           'default_response': '我这东西可难做了，池翔云好厉害，可惜还是需要更多的时间培养，要耐心哦~'
       }
   ],
   trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
 )
 
-english_bot.train("chatterbot.corpus.english")
-english_bot.train("chatterbot.corpus.chinese")
+zhuzhu_bot.train("chatterbot.corpus.english")
+zhuzhu_bot.train("chatterbot.corpus.chinese")
+
+zhuzhu_bot.set_trainer(ListTrainer)
+zhuzhu_bot.train(data)
 
 @app.route("/")
 def home():
@@ -27,7 +41,7 @@ def home():
 
 @app.route("/get/<string:query>")
 def get_raw_response(query):
-    return str(english_bot.get_response(query))
+    return str(zhuzhu_bot.get_response(query))
 
 
 if __name__ == "__main__":
